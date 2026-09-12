deb9be575b034efaae071e65c3e8eda18af8db9a
marcmyworld <moviesminister@gmail.com>
2026-09-12 12:50:37 +0530

    chenfeng: Integrate LunarisDolby and enable Dolby Atmos audio
    - Inherit hardware/dolby/dolby.mk with TARGET_PROVIDES_DOLBY_BLOBS := true
    - Enable vendor Dolby DAX tuning files (dax-default.xml, dax-default-spatializer.xml)
    - Add Dolby vendor audio properties matching stock DAX3_3.8.5.20_r1
    - Allow system_app and platform_app client access to hal_dms

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,9 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 # Hardware
 BOARD_USES_QCOM_HARDWARE := true
 
+# Dolby
+TARGET_PROVIDES_DOLBY_BLOBS := true
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/device_framework_matrix.xml \
@@ -80,7 +83,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/hidl/manifest_vendor.xml \
     hardware/qcom-caf/sm8650/audio/primary-hal/configs/common/manifest_non_qmaa.xml \
     hardware/qcom-caf/sm8650/audio/primary-hal/configs/common/manifest_non_qmaa_extn.xml
--- device.mk
+++ device.mk
@@ -160,12 +160,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/device_features/chenfeng.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/chenfeng.xml
 
 # Dolby
-# PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
-    $(LOCAL_PATH)/configs/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default-spatializer.xml \
+    $(LOCAL_PATH)/configs/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default-spatializer.xml
 
-# PRODUCT_PACKAGES += \
-    DolbyAtmos
+TARGET_PROVIDES_DOLBY_BLOBS := true
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
